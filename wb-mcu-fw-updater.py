bc79d0e2ed40affca7333ca24a0f1f9469226d21
vdromanov <vdromanov@yahoo.com>
2020-04-30 12:35:56 +0300

    sphinx-style comments

--- wb-mcu-fw-updater.py
+++ wb-mcu-fw-updater.py
@@ -9,9 +9,15 @@ if __name__ == "__main__":
     branch_name = 'feature-th_error_cnt'
 
     stable_file_watcher = RemoteFileWatcher()
-    logging.debug('Latest fw (by raw link): %s' % stable_file_watcher._get_latest_fw_version('http://fw-releases.wirenboard.com/fw/by-signature/msw3-48mh/stable/latest.txt')) # By raw link
-    logging.debug('Latest fw (by signature): %s' % stable_file_watcher.get_latest_fw_version(device_signature)) # By device's signature
-    stable_file_watcher.get_fw_file(device_signature)
+    logging.debug('Latest fw (by raw link): %s' % stable_file_watcher._get_request_content('http://fw-releases.wirenboard.com/fw/by-signature/msw3-48mh/stable/latest.txt')) # By raw link
+    logging.debug('Latest fw (by signature): %s' % stable_file_watcher.get_latest_version_number(device_signature)) # By device's signature
+    stable_file_watcher.download(device_signature)
 
     unstable_file_watcher = RemoteFileWatcher(branch_name=branch_name)
-    unstable_file_watcher.get_fw_file(device_signature)
+    unstable_file_watcher.download(device_signature)
+
+    stable_bootloader = RemoteFileWatcher(mode='bootloader')
+    latest_bootloader_version = stable_bootloader.get_latest_version_number(device_signature)
+    logging.debug('Latest bootloader (by signature): %s' % latest_bootloader_version) # By device's signature
+    stable_bootloader.download(device_signature, version=latest_bootloader_version)
+    stable_bootloader.download(device_signature, version=latest_bootloader_version, fname='latest_bootloader.wbfw')
--- wb_mcu_fw_updater/fw_downloader.py
+++ wb_mcu_fw_updater/fw_downloader.py
@@ -5,20 +5,32 @@ from posixpath import join as urljoin
 from . import die
 
 
-ROOT_URL = 'http://fw-releases.wirenboard.com/'
+ROOT_URL = 'http://fw-releases.wirenboard.com/' #TODO: fill from config/env vars
 FW_SAVING_DIR = os.path.join(os.path.dirname(__file__), 'firmwares')
 
 
 class RemoteFileWatcher(object):
+    """
+    A class, downloading Firmware from remote server.
+
+    """
     _EXTENSION = '.wbfw'
     _LATEST_FW_VERSION_FILE = 'latest.txt'
     _LATEST_FW_FILE = 'latest.wbfw'
     _SOURCE = 'stable'
     _BRANCH = ''
-    """
-    Downloading WB-Devices FW from remote amazon storage
-    """
+
     def __init__(self, mode='fw', sort_by='by-signature', branch_name=None):
+        """
+        Could download firmware or bootloder files from stable or specified branch.
+
+        :param mode: firmware or bootloader, defaults to 'fw'
+        :type mode: str, optional
+        :param sort_by: files on remote server are stored by project_name or device_signature, defaults to 'by-signature'
+        :type sort_by: str, optional
+        :param branch_name: looking for fw/bootloader from specified branch (instead of stable), defaults to None
+        :type branch_name: str, optional
+        """
         if self._check_url_is_available(ROOT_URL):
             self.parrent_url_path = urljoin(ROOT_URL, mode, sort_by) 
         else:
@@ -28,46 +40,83 @@ class RemoteFileWatcher(object):
             logging.debug('Looking to unstable branch: %s' % branch_name)
             self._SOURCE = urljoin('unstable', branch_name)
 
-    def _check_url_is_available(self, url_path):
+    def _check_url_is_available(self, url_path, max_allowed_retcode=400):
         """
-        Sending HEAD reauest to <url_path> and looking into http status code
-        """
-        logging.debug('Checking url: %s' % url_path)
-        _max_allowed_http_code = 400
-        ret = requests.head(url_path)
-        return True if ret.status_code < _max_allowed_http_code else False
+        Checking url accessibility by sending HEAD request to it and anallyzing http return code.
 
-    def _get_latest_fw_version(self, url_path):
-        """
-        Latest FW version number is contained into <url>/<_LATEST_FW_LOCATION> (ex: ***/***/***/latest.txt)
-        Getting from full url path
+        :param url_path: url, need to be checked
+        :type url_path: str
+        :param max_allowed_retcode: http request code should be less, than. Defaults to 400
+        :type max_allowed_retcode: int, optional
+        :return: has url accessible or not
+        :rtype: bool
         """
         logging.debug('Checking url: %s' % url_path)
+        ret = requests.head(url_path)
+        return True if ret.status_code < max_allowed_retcode else False
+
+    def _get_request_content(self, url_path):
+        """
+        Checking, is url_path available; sending GET request to it; returning responce's content.
+
+        :param url_path: url, request will be sent to
+        :type url_path: str
+        :return: responce's content
+        :rtype: bytestring
+        """
         if self._check_url_is_available(url_path):
             ret = requests.get(url_path)
-            return ret.content
+            return ret.content.strip()
         else:
             die('Wrong url: %s' % url_path)
 
-    def _make_full_url(self, name):
+    def _construct_urlpath(self, name):
+        """
+        Appending url from parts (parent url, significant part, stable or feature branch), excepting filename.
+
+        :param name: a significant part of url. Could be a project_name or device_signature
+        :type name: str
+        :return: constructed url without filename
+        :rtype: str
+        """
         return urljoin(self.parrent_url_path, name, self._SOURCE)
 
-    def get_latest_fw_version(self, name):
-        url_path = urljoin(self._make_full_url(name), self._LATEST_FW_VERSION_FILE)
-        return self._get_latest_fw_version(url_path)
+    def get_latest_version_number(self, name):
+        """
+        Latest fw or bootloader version number is stored into a text file on server.
 
-    def get_fw_file(self, name, version='latest', fname=None):
-        if not os.path.isdir(FW_SAVING_DIR):
-            os.mkdir(FW_SAVING_DIR)
+        :param name: could be a device_signature or project_name
+        :type name: str
+        :return: content of text file, where latest fw version number is stored
+        :rtype: bytestring
+        """
+        url_path = urljoin(self._construct_urlpath(name), self._LATEST_FW_VERSION_FILE)
+        return self._get_request_content(url_path)
+
+    def download(self, name, version='latest', fname=None):
+        """
+        Downloading a firmware/bootloader file with specified version to specified fname.
+
+        :param name: a project_name or device_signature
+        :type name: str
+        :param version: specified fw/bootloader version, defaults to 'latest'
+        :type version: str, optional
+        :param fname: custom path, file will be saved, defaults to None
+        :type fname: str, optional
+        :return: path of saved file
+        :rtype: str
+        """
         fw_ver = '%s%s' % (version, self._EXTENSION)
-        url_path = urljoin(self._make_full_url(name), fw_ver)
-        if self._check_url_is_available(url_path):
+        url_path = urljoin(self._construct_urlpath(name), fw_ver)
+        content = self._get_request_content(url_path)
+        if not fname:
+            if not os.path.isdir(FW_SAVING_DIR):
+                os.mkdir(FW_SAVING_DIR)
             fname = '%s_%s_%s' % (name, self._BRANCH, fw_ver)
             fpath = os.path.join(FW_SAVING_DIR, fname)
-            logging.debug('Downloading to: %s' % fpath)
-            ret = requests.get(url_path)
-            with open(fpath, 'wb+') as fh:
-                fh.write(ret.content)
         else:
-            die('Url %s is not available. Check params!' % url_path)
+            fpath = fname
+        logging.debug('Downloading to: %s' % fpath)
+        with open(fpath, 'wb+') as fh:
+            fh.write(content)
         return fpath
