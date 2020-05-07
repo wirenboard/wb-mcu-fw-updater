5d9e4dc236b329be8a6cd0765b274599eb1d08f3
vdromanov <vdromanov@yahoo.com>
2020-05-07 18:00:46 +0300

    fixed shebang-auto-changing problem: installing executable script via distutils; forced calling setup.py with py2 and py3 at build

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(name          =   "wb-mcu-fw-updater",
       version       =   "1.0",
       author        =   "Vladimir Romanov",
       description   =   "Wiren Board modbus devices firmware update tool",
-      packages      =   find_packages())
+      url           =   "https://github.com/wirenboard/wb-mcu-fw-updater",
+      packages      =   ['wb_mcu_fw_updater', 'wb_modbus'],
+      scripts       =   ['wb-mcu-fw-updater']
+)
