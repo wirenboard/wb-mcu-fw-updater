b80576ff275ad2cbb3a8c4fc690767f0f047f0da
vdromanov <vdromanov@yahoo.com>
2022-08-01 11:10:30 +0300

    wb_modbus.bindings: handle decode error, when reading from modbus regs

--- wb_modbus/bindings.py
+++ wb_modbus/bindings.py
@@ -450,7 +450,11 @@ class MinimalModbusAPIWrapper(object):
         ret = minimalmodbus._hexlify(self.device.read_string(addr, regs_lenght, 3))
         for placeholder in empty_chars_placeholders:  # Clearing a string to only meaningful bytes
             ret = ret.replace(placeholder, '')  # 'A1B2C3' bytes-only string
-        return str(unhexlify(ret).decode('utf-8')).strip()
+        try:
+            return str(unhexlify(ret).decode('utf-8')).strip()
+        except UnicodeDecodeError as e:
+            logger.exception(e)
+            return ''
 
 
 def auto_find_uart_settings(method_to_decorate):
