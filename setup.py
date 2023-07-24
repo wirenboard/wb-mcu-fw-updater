48c905f7023769ed1e4ef9a4f780d564644af4dc
Nikolay Korotkiy <sikmir@disroot.org>
2023-07-24 17:03:49 +0400

    Fix pkg-info (#47)

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@
 
 from distutils.core import setup
 
+
+def get_version():
+    with open("debian/changelog", "r", encoding="utf-8") as f:
+        return f.readline().split()[1][1:-1]
+
+
 setup(
     name="wb-mcu-fw-updater",
-    version="1.0",
+    version=get_version(),
     author="Vladimir Romanov",
     author_email="v.romanov@wirenboard.ru",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
     description="Wiren Board modbus devices firmware update tool",
+    license="MIT",
     url="https://github.com/wirenboard/wb-mcu-fw-updater",
     packages=["wb_mcu_fw_updater", "wb_modbus"],
 )
