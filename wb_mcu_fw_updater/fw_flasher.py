4a3fb60d66c3bfc3fd49e50733647013fb955eed
vdromanov <vdromanov@yahoo.com>
2024-01-09 13:44:46 +0300

    check, will userdata be reset during update or not

--- wb_mcu_fw_updater/fw_flasher.py
+++ wb_mcu_fw_updater/fw_flasher.py
@@ -28,6 +28,60 @@ class BootloaderCmdError(FlashingError):
     pass
 
 
+class ParsedWBFW:
+    INFO_BLOCK_LENGTH_U16_REGS = 16
+    DATA_BLOCK_LENGTH_U16_regs = 68
+
+    def __init__(self, wbfw_fpath):
+        self.fpath = wbfw_fpath
+        self._info_values = []
+        self._data_chunks = []
+        self.parse_wbfw(wbfw_fpath)
+
+    def __str__(self):
+        return f"{self.fpath} ({len(self._data_chunks)} data chunks)"
+
+    @property
+    def info(self):
+        return self._info_values
+
+    @property
+    def data_chunks(self):
+        return self._data_chunks
+
+    def parse_wbfw(self, fw_fpath):
+        """
+        Converting fw file contents to a bootloader-suitable format:
+            - info: a row of u16 regs
+            - data: N chunks of u16-regs rows
+        """
+        bs = int(os.path.getsize(fw_fpath))
+        if bs % 2:
+            raise IncorrectFwError("Fw file should be even-bytes long!\nGot %s (%db)" % (fw_fpath, bs))
+
+        u16_regs = []
+        with open(fw_fpath, "rb") as fp:
+            raw_bytes = fp.read()
+            bytestr = str(raw_bytes, encoding="latin1")
+            try:
+                u16_regs = minimalmodbus._bytestring_to_valuelist(bytestr, int(bs / 2))
+            except (TypeError, ValueError) as e:
+                raise IncorrectFwError from e
+
+        self._info_values, data_values = (
+            u16_regs[: self.INFO_BLOCK_LENGTH_U16_REGS],
+            u16_regs[self.INFO_BLOCK_LENGTH_U16_REGS :],
+        )
+        if len(self._info_values) != self.INFO_BLOCK_LENGTH_U16_REGS:
+            raise IncorrectFwError(
+                f"Info block size should be {self.INFO_BLOCK_LENGTH_U16_REGS} regs! Got {len(self._info_values)} instead\nRaw regs: {self._info_values}"
+            )
+        self._data_chunks = [
+            data_values[i : i + self.DATA_BLOCK_LENGTH_U16_regs]
+            for i in range(0, len(data_values), self.DATA_BLOCK_LENGTH_U16_regs)
+        ]
+
+
 class ModbusInBlFlasher(object):
     """
     Interacting with WirenBoard Modbus device's bootloader:
@@ -37,13 +91,13 @@ class ModbusInBlFlasher(object):
     """
 
     INFO_BLOCK_START = 0x1000
-    INFO_BLOCK_LENGTH = 16
     DATA_BLOCK_START = 0x2000
-    DATA_BLOCK_LENGTH = 68
 
     # u16 holdings (available only in bootloader; write "1" to perform a cmd)
     UART_SETTINGS_RESET_REG = 1000  # in-bl only
     EEPROM_ERASE_REG = 1001  # in-bl only
+
+    GET_FREE_SPACE_FLASHFS_REG = 1003
 
     MINIMAL_RESPONSE_TIMEOUT = 5.0  # should be relatively huge (for wireless devices)
 
@@ -63,38 +117,11 @@ class ModbusInBlFlasher(object):
         self._actual_response_timeout = max(self.MINIMAL_RESPONSE_TIMEOUT, response_timeout)
         self.instrument.set_response_timeout(self._actual_response_timeout)
 
-    def _read_to_u16s(self, fw_fpath):
-        """
-        converting fw file contents to a row of u16 modbus regs
-        """
-        coding = "latin1"
-
-        bs = int(os.path.getsize(fw_fpath))
-        if bs % 2:
-            raise IncorrectFwError("Fw file should be even-bytes long!\nGot %s (%db)" % (fw_fpath, bs))
-
-        with open(fw_fpath, "rb") as fp:
-            raw_bytes = fp.read()
-            if six.PY2:
-                bytestr = raw_bytes.decode(coding).encode(coding)
-            else:
-                bytestr = str(raw_bytes, encoding=coding)
-            try:
-                return minimalmodbus._bytestring_to_valuelist(bytestr, int(bs / 2))  # u16
-            except Exception as e:
-                six.raise_from(IncorrectFwError, e)
-
     def _send_info(self, regs_row):
         """
         Writing INFO block as u16 regs
         Writing correct INFO block triggers some in-device hidden magic (leads to delay in device's response)
         """
-        if len(regs_row) != self.INFO_BLOCK_LENGTH:
-            raise IncorrectFwError(
-                "Info block size should be %d regs! Got %d instead\nRaw regs: %s"
-                % (self.INFO_BLOCK_LENGTH, len(regs_row), str(regs_row))
-            )
-
         try:
             self.instrument.set_response_timeout(
                 self._actual_response_timeout + self.instrument.BOOTLOADER_INFOBLOCK_MAGIC_TIMEOUT
@@ -107,13 +134,10 @@ class ModbusInBlFlasher(object):
         finally:
             self.instrument.set_response_timeout(self._actual_response_timeout)
 
-    def _send_data(self, regs_row):
+    def _send_data(self, chunks):
         """
         Writing DATA block as u16 regs (split into fixed length chunks)
         """
-        chunk_size = self.DATA_BLOCK_LENGTH  # bootloader accepts only fixed-length chunks
-        chunks = [regs_row[i : i + chunk_size] for i in range(0, len(regs_row), chunk_size)]
-
         has_previous_chunk_failed = False  # Due to bootloader's behaviour, actual flashing failure is current-chunk failure + next-chunk failure
         for chunk in tqdm(chunks, ascii=True, dynamic_ncols=True, bar_format="{l_bar}{bar}|{n}/{total}"):
             try:
@@ -149,13 +173,24 @@ class ModbusInBlFlasher(object):
         logger.debug("Resetting all device's settings")
         self._perform_bootloader_cmd(self.EEPROM_ERASE_REG)
 
-    def flash_in_bl(self, fw_fpath):
+    def is_userdata_preserved(self, parsed_wbfw: ParsedWBFW):
+        """
+        If device's flashfs has not enough space, user data (such as ir commands)
+        will be erased to perform fw update
+        """
+        device_str = f"{self.instrument.slaveid}, {self.instrument.port}"
+        try:
+            available_chunks_fs = self.instrument.read_u16(self.GET_FREE_SPACE_FLASHFS_REG)
+            logger.debug("Device (%s) has available space of %d chunks", device_str, available_chunks_fs)
+        except minimalmodbus.ModbusException:
+            logger.exception(f"Treating device ({device_str}) as erasing user data!")
+            return False
+        return available_chunks_fs > len(parsed_wbfw.data_chunks)
+
+    def flash_in_bl(self, parsed_wbfw: ParsedWBFW):
         """
         Writing fw to device as u16 regs; device should be in bootloader mode!
         """
-        fw_as_regs = self._read_to_u16s(fw_fpath)
-        info_block, data_block = fw_as_regs[: self.INFO_BLOCK_LENGTH], fw_as_regs[self.INFO_BLOCK_LENGTH :]
-
-        logger.info("Flashing %s", fw_fpath)
-        self._send_info(info_block)
-        self._send_data(data_block)
+        logger.info("Flashing %s", str(parsed_wbfw))
+        self._send_info(parsed_wbfw.info)
+        self._send_data(parsed_wbfw.data_chunks)
